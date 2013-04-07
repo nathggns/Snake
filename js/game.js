6b0ff9223fdc4391807cf6e1ba026bff91d893b0
Nathaniel Higgins <nat@nath.is>
2013-04-07 22:32:56 +0100

    Keep track of objects in the game.

--- js/game.js
+++ js/game.js
@@ -1,6 +1,21 @@
 var Game = (function(window, document, undefined) {
 
-  var Game = function Game() {};
+  var Game = function Game(canvas) {
+    this.canvas = canvas;
+    this.objects = [];
+  };
+
+  this.add = function(obj) {
+    this.objects.push(obj);
+
+    return this;
+  };
+
+  this.remove = function(obj) {
+    this.objects.splice(this.objects.indexOf(obj), 1);
+
+    return this;
+  };
 
   return Game;
 
