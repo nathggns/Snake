fbf0da4ff7f0be11431d35f158faae06bd05c8de
Nathaniel Higgins <nat@nath.is>
2013-04-07 22:03:21 +0100

    Add page-ready boilerplate to page.js.

---
+++ js/page.js
@@ -0,0 +1,14 @@
+(function(window, document, ready, undefined) {
+
+    if (typeof ready === 'undefined') {
+        var args = arguments;
+        var callee = arguments.callee;
+        var me = this;
+
+        return document.addEventListener('DOMContentLoaded', function() {
+            Array.prototype.push.call(args, true);
+            return callee.apply(me, args);
+        });
+    }
+
+})(this, this.document);
