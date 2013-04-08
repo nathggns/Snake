ff20c4a1fe8af3b6539f6b4cf326223d6303aeb3
Nathaniel Higgins <nat@nath.is>
2013-04-08 10:38:48 +0100

    Update some more stuff

--- js/page.js
+++ js/page.js
@@ -31,11 +31,25 @@
     };
 
     Background.prototype.render = function(ctx) {
-        ctx.beginPath();
-        ctx.fillStyle = this.background;
-        ctx.rect(0, 0, this.game.game.width, this.game.game.height);
-        ctx.fillStyle = '#ffffff';
-        ctx.fill();
+        var color = '#ffffff';
+
+        for (var i = 0, l = this.game.game.width / 70; i < l; i++) {
+
+            for (j = 0, k = this.game.game.height / 70; j < k; j++) {
+                ctx.beginPath();
+                ctx.fillStyle = this.background;
+                ctx.rect(70 * i, 70 * j, 70, 70);
+                ctx.fillStyle = color;
+                ctx.fill();
+
+                
+                if (color === '#ffffff') {
+                    color = '#dddddd';
+                } else {
+                    color = '#ffffff';
+                }
+            }
+        }
     };
 
     return Background;
@@ -45,11 +59,11 @@
 
     var Player = function Player() {
         this.order = 2;
-        this.width = this.height = 50;
+        this.width = this.height = 70;
 
         this.speed = 2;
 
-        this.direction = ['y', 1];
+        this.direction = ['y', 1, 'height'];
     };
 
     Player.prototype.init = function() {
@@ -58,20 +72,33 @@
     };
 
     Player.prototype.update = function() {
+
+        window.player = this;
+
+        var player = this;
+        var direction;
+
         if (37 in game.keys) {
-            this.direction = ['x', -1];
+            direction = ['x', -1, 'width'];
         }
 
         if (39 in game.keys) {
-            this.direction = ['x', 1];
+            direction = ['x', 1, 'width'];
         }
 
         if (38 in game.keys) {
-            this.direction = ['y', -1];
+            direction = ['y', -1, 'height'];
         }
 
         if (40 in game.keys) {
-            this.direction = ['y', 1];
+            direction = ['y', 1, 'height'];
+        }
+
+        if (direction) {
+            (function() {
+                if (direction[0] === player.direction[0]) return;
+                player.direction = direction;
+            })();
         }
 
         this[this.direction[0]] += this.speed * this.direction[1];
@@ -92,8 +119,6 @@
     Player.prototype.render = function(ctx) {
         ctx.beginPath();
         ctx.fillStyle = '#aaaaaa';
-
-
 
         ctx.rect(this.x, this.y, this.width, this.height);
         ctx.fill();
