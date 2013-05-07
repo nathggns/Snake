ccd685a3bdf64210d4e636ecdf1d97f7e37728c5
Nathaniel Higgins <nat@nath.is>
2013-05-07 23:02:21 +0100

    Fix logging.

--- app.js
+++ app.js
@@ -11,7 +11,22 @@ var fs = require('fs');
 
 var app = express();
 
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
 // all environments
+
+
+app.configure('development', function() {
+    app.use(express.errorHandler());
+     app.use(express.logger('dev'));
+    io.set('log level', 3);
+});
+
+app.configure('production', function() {
+    io.set('log level', 0);
+});
+
 app.configure(function() {
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
@@ -27,7 +42,6 @@ app.configure(function() {
         }
     });
     app.use(express.favicon());
-    app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
@@ -35,14 +49,7 @@ app.configure(function() {
     app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function() {
-    app.use(express.errorHandler());
-});
-
 app.get('/', routes.index);
-
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
 
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
